41c90230603c94d6ce623791c5458ed3a0e65e7e
ICAN0007 <icankissyou0007@gmail.com>
2025-06-06 15:48:06 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -215,12 +215,83 @@ const videos = [
     hashtags: ["SlowUndress", "DesiGirl", "FullNude", "IndianTease"],
     views: "5.5k"
   },
-   {
+  {
     title: "This hotel room smelled of dick and wet pussy all night long",
     url: "https://files.catbox.moe/4l4mh5.mp4",
     thumbnail: "https://files.catbox.moe/dcdqz8.png",
-    categories: [ "Brazzer"],
+    categories: [ "Brazzers"],
     hashtags: ["Amateur porn", "Hardcore"],
     views: "7.5k"
-  }
+  },
+  {
+  title: "Bella is a stepsister any stepbrother could ever dream of",
+  url: "https://files.catbox.moe/z0ig0s.mp4", // Placeholder, please provide actual URL
+  thumbnail: "https://files.catbox.moe/tkxz24.png", // Placeholder, please provide actual thumbnail
+  categories: ["Brazzers"], // Assumed, as "Brazzer" isn’t in original categories
+  hashtags: ["Black Cocks", "Huge cocks", "Stepsister Stepbrother", "Young 18 year old"],
+  views: "5.8k" // Assumed, as no views provided
+},
+{
+  title: "Sexy blonde hooked up with a gorgeous fuckboy and had sensational anal sex",
+  url: "https://files.catbox.moe/idzjb5.mp4",
+  thumbnail: "https://files.catbox.moe/0goda1.jpg",
+  categories: ["Vixen"], // Assumed, as "TushyRaw" isn’t in original categories
+  hashtags: ["Anal sex"],
+  views: "3.7k" // Assumed, as no views provided
+},
+{
+  title: "The second date grew into steamy public sex with a kinky chick",
+  url: "https://files.catbox.moe/afn4mh.mp4", // Placeholder, please provide actual URL
+  thumbnail: "https://files.catbox.moe/mw1fzd.png", // Placeholder, please provide actual thumbnail
+  categories: ["Vixen"], // Assumed, as "Deeeper" isn’t in original categories
+  hashtags: ["Hot Girls", "Young 18 year old"],
+  views: "4.7k" // Assumed, as no views provided
+},
+{
+  title: "24 Hours",
+  url: "https://files.catbox.moe/1defn5.mp4",
+  thumbnail: "https://files.catbox.moe/epwbfb.png",
+  categories: ["Vixen"], // Assumed, as no category provided
+  hashtags: ["Threesome", "Sexy Girls", "Hardcore"],
+  views: "7.2k" // Assumed, as no views provided
+},
+{
+  title: "This cleaner didn’t expect a threesome as a part of her job",
+  url: "https://files.catbox.moe/twmh5i.mp4",
+  thumbnail: "https://files.catbox.moe/fiadbm.png",
+  categories: ["Brazzers"],
+  hashtags: ["Threesome"],
+  views: "5.8k"
+},
+{
+  title: "SHE COULDN’T CONTAIN HER EXCITEMENT AT THE TRIP",
+  url: "https://files.catbox.moe/8tqjpa.mp4",
+  thumbnail: "https://files.catbox.moe/8x0og0.png",
+  categories: ["Vixen"],
+  hashtags: ["Sneaky sex", "Hardcore", "Anal Sex"],
+  views: "4.9k"
+},
+{
+  title: "Naughty nurse takes care of her patient’s hard cock",
+  url: "https://files.catbox.moe/leivs9.mp4",
+  thumbnail: "https://files.catbox.moe/prszz0.png",
+  categories: ["Brazzers"],
+  hashtags: ["Anal Sex", "Young 18 year old"],
+  views: "6.3k"
+},
+{
+  title: "Spoiled rich girl spends her days excitedly engaging in the sexual games",
+  url: "https://files.catbox.moe/otw0qt.mp4",
+  thumbnail: "https://files.catbox.moe/1b1f7x.png",
+  categories: ["Vixen"],
+  hashtags: ["Lesbians"],
+  views: "5.4k"
+},
+{
+  title: "Cock-hungry nymphos find the perfect guy to ravage them in a threesome",
+  url: "https://files.catbox.moe/kwu0vd.mp4",
+  thumbnail: "https://files.catbox.moe/a7lf34.jpg",
+  categories: ["Brazzers"],
+  hashtags: [ "Threesome", "Sneaky sex"],
+  views: "7.8k"
 ];
